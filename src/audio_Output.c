043e02a20a5e1b6101a953b50e22eee4fe8c8d50
Tony Josi <tonyjosinew@gmail.com>
2021-02-07 13:20:58 +0530

    audio init

--- src/audio_Output.c
+++ src/audio_Output.c
@@ -18,27 +18,47 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <assert.h>
 
 #include "../inc/audio_Output.h"
+
+#define __NULL_POINTER_CHECK(ptr)   assert(ptr != NULL)
 
 void audio_Out_Callback(void* data, Uint8 *stream, int len);
 
 typedef struct __AUDIO_OP_INTERNAL {
 
     uint8_t                 stop_Thread;
-    pthread_mutex_t        *lock_Ptr;
-    SDL_AudioSpec          *ao_Spec_ptr;
+    Audio_Out_t            *ao_Out_Obj_ptr;
 
 } Audio_OP_Internal_t;
 
+static Audio_OP_Internal_t __gs_Audio_Obj;
 
 Audio_Out_Status_t audio_Out_Init(Audio_Out_t *ao_user_Obj) {
+
+    __NULL_POINTER_CHECK(ao_user_Obj);
 
     SDL_AudioSpec *__audio_Spec_Ptr = malloc(sizeof(SDL_AudioSpec));
     if(__audio_Spec_Ptr == NULL) goto __Free_mem_N_Return;
 
-    __Free_mem_N_Return:
-        
+    __audio_Spec_Ptr->freq = ao_user_Obj->sampling_Freq;
+    __audio_Spec_Ptr->format = AUDIO_U8;
+    __audio_Spec_Ptr->samples = ao_user_Obj->samples_Per_Frame;
+    __audio_Spec_Ptr->callback = audio_Out_Callback;
+    __audio_Spec_Ptr->userdata = ao_user_Obj;
+
+    if(SDL_OpenAudio(__audio_Spec_Ptr, NULL) != 0) 
+        return AUDIO_OUT_FAIL; 
+
+    __gs_Audio_Obj.stop_Thread = 1;
+    __gs_Audio_Obj.ao_Out_Obj_ptr = ao_user_Obj;
+
+    return AUDIO_OUT_OK;
+
+__Free_mem_N_Return:
+    free(__audio_Spec_Ptr);
+    return AUDIO_OUT_FAIL;
 
 }
 
