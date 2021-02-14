d7d4cc6ca8f8ceb4ecd2ceab35e769b903b25751
Tony Josi <tonyjosinew@gmail.com>
2021-02-15 00:52:03 +0530

    spi test hal file

--- test/spi_hal.c
+++ test/spi_hal.c
@@ -17,10 +17,20 @@
 #define _T_SPI_MODE            (SPI_NO_CS | SPI_RX_OCTAL | _T_SPI_CPHA_CPOL)
 #define _T_SPI_DATA_RATE       (1000000U)
 #define _T_SPI_NO_OF_BITS      (8)
+#define _T_SPI_DEVICE_NAME      "/dev/spidev0.1"
 
 static int SPI_HAL_ReceiveData(int spi_fd, uint8_t *rx_buff, size_t data_len);
 
 int main() {
+
+    int spi_Handle;
+    spi_Handle = open(_T_SPI_DEVICE_NAME, O_RDWR);
+    if(spi_Handle < 0) {
+        printf("Error opening device\n");
+        return 0;
+    }
+
+    
 
     return 0;
 }
