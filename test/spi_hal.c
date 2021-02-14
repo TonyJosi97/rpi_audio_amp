74f82ee6ecd0fb4c915119a3814cd8012b606de9
Tony Josi <tonyjosinew@gmail.com>
2021-02-15 00:56:00 +0530

    spi test hal file

--- test/spi_hal.c
+++ test/spi_hal.c
@@ -19,6 +19,10 @@
 #define _T_SPI_NO_OF_BITS      (8)
 #define _T_SPI_DEVICE_NAME      "/dev/spidev0.1"
 
+static uint32_t     _spi__Mode          = _T_SPI_MODE;
+static uint32_t     _spi__DataRate      = _T_SPI_DATA_RATE;
+static uint8_t      _spi__NoOfBits      = _T_SPI_NO_OF_BITS;
+
 static int SPI_HAL_ReceiveData(int spi_fd, uint8_t *rx_buff, size_t data_len);
 
 int main() {
@@ -30,7 +34,8 @@ int main() {
         return 0;
     }
 
-    
+    int ret_Code = 0;
+    ret_Code = ioctl(spi_Handle, SPI_IOC_WR_MODE32, )
 
     return 0;
 }
